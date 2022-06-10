f086ea7ac5a03f972a52f89d4c9c338312175618
AndriiGriMM <webdevgrimm@gmail.com>
2022-06-10 23:03:49 +0300

    homework  10.06.22

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
-let form = document.querySelector('.form')
-let submit = document.querySelector('.js--submit')
-let from = form.querySelector('.js--number')
-let fields = form.querySelectorAll('.input')
 
+const from = document.querySelector('.from')
+const test = document.querySelector('.test')
+const case1 = test.querySelector('.case1')
+const case2 = test.querySelector('.case2')
+const case3 = test.querySelector('.case3')
+const case4 = test.querySelector('.case4')
+const regLetter = /[A-ZА-ЯЁ]/g
+const regNumber = /\d/g
+const regSymbol = /[.,:;()]/g
+const regAmount = /.{8}/g
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
-    let errors= form.querySelectorAll('.error')
+from.addEventListener('input', function (event) {
+  const input = this.value
+  function validate (argument,reg){
+    input.match(reg)?argument.style.color = "red":argument.style.color = "black"
+  }
+  validate (case1,regLetter)
+  validate (case2,regNumber)
+  validate (case3,regSymbol)
+  validate (case4,regAmount)
 
-    for (let i = 0; i < errors.length; i++) {
-        errors[i].remove()
-    }
-    
-    for (let i = 0; i < fields.length; i++) {
-       if (!fields[i.value]){
-        console.log("fields is blank", fields[i])
-        let error = document.createElement('div')
-        error.className="error"
-        from.style.border = "1px solid red"
-        error.innerHTML= "cennot be blank"
-        form[i].parentElement.insertBefore(error, fields[i])
-       }
-    }
-})
-
-
-from.addEventListener('input',function (arg) {
-    this.value = Number( this.value.substr(0,15))
-    if(isNaN(this.value)) {
-       
-    }
 })
